f7d7283886137a5b7fc0acb2b63fb5f9bf2416b7
Ali Musa <alimusa_@abv.bg>
2024-05-01 21:31:42 +0300

    Added Categories and modified Edit, Index and Layout

--- WebApp/Controllers/CategoriesController.cs
+++ WebApp/Controllers/CategoriesController.cs
@@ -7,14 +7,54 @@ namespace WebApp.Controllers
     {
         public IActionResult Index()
         {
+            var categories = CategoriesRepository.GetCategories();
+            return View(categories);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            ViewBag.Action = "edit";
+
+            var category = CategoriesRepository.GetCategoryById(id.HasValue?id.Value:0);
+
+            return View(category);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                CategoriesRepository.UpdateCategory(category.CategoryId, category);
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(category);
+        }
+
+        public IActionResult Add()
+        {
+            ViewBag.Action = "add";
+
             return View();
         }
 
-        public IActionResult Edit(int? id) 
+        [HttpPost]
+        public IActionResult Add(Category category)
         {
-            var category = new Category { CategoryId = id.HasValue?id.Value:0 };
+            if (ModelState.IsValid)
+            {
+                CategoriesRepository.AddCategory(category);
+                return RedirectToAction(nameof(Index));
+            }
 
             return View(category);
+        }
+
+        public IActionResult Delete(int categoryId)
+        {
+            CategoriesRepository.DeleteCategory(categoryId);
+            return RedirectToAction(nameof(Index));
         }
 
     }
